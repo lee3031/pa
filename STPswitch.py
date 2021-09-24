709fc01a3576e6e40a097a7c3a536bf262dfe145
Marvin Mui <marvinhmui@gmail.com>
2021-09-23 22:23:02 -0400

    Implemented periodic operation

--- STPswitch.py
+++ STPswitch.py
@@ -17,11 +17,12 @@ class STPswitch(Switch):
         Switch.__init__(self, addr, heartbeatTime)  # initialize superclass - don't remove
         """TODO: add your own class fields and initialization code here"""
         self.forwardingTable = {}
+        self.controlPacketValues = [self.addr, 0, self.addr, self.addr]
 
     def handlePacket(self, port, packet):
         """TODO: process incoming packet"""
         # default implementation sends packet back out the port it arrived
-        """if packet.isData():
+        if packet.isData():
             if packet.dstAddr != "X":
                 pass
             else:
@@ -29,7 +30,14 @@ class STPswitch(Switch):
                 for p in self.links.keys():
                     if p != port:
                         print("Broadcast packet sent on port {}".format(p))
-                        self.send(p, packet)"""
+                        self.send(p, packet)
+        elif packet.isControl():
+            content = self.controlPacketContentToValue(packet.content)
+            if content[0] < self.controlPacketValues[0]:
+                self.controlPacketValues[0] = content[0]
+                self.controlPacketValues[1] = content[1] + 1
+                self.controlPacketValues[3] = content[2]
+
         self.send(port, packet)
 
     def handleNewLink(self, port, endpoint, cost):
@@ -52,4 +60,23 @@ class STPswitch(Switch):
                                 |___|
                             
         '''
+
+        controlPacketContent = (self.controlPacketToString(self.controlPacketValues))
+        controlPacket = Packet(2, self.addr, 'X', controlPacketContent)
+        for port in self.links:
+            if type(self.links[port].e2) != 'Client':
+                self.send(port, controlPacket)
+
         pass
+
+    def controlPacketToString(self, controlPacket):
+        s = ""
+        for i in self.controlPacketValues:
+            s += str(i)
+        return s
+
+    def controlPacketContentToValue(self, content):
+        l = []
+        for character in content:
+            l.append(int(character))
+        return l
